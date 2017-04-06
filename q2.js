a2217cf9eca39c629b8cb242f4d4f9b57c7abf85
vianali <vian.ali47@gmail.com>
2017-04-06 11:42:20 +0000

    add q2

---
+++ q2.js
@@ -0,0 +1,6 @@
+// 2. Repeat the program in 1, except, starting from the end of the array and traversing backwards.
+// 1. Write the following programs:
+//  - Write a JavaScript program that will loop through array a, and capitalizes every third word.
+//  - Create a Ruby program that does exactly the same thing.
+
+var a=["john","mike","susan","matt","joe","larry","ken","samantha","jenny","Sara","henry"];

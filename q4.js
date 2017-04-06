708e4e4e7b3a6f694992ef33e203cd1457aa8c21
vianali <vian.ali47@gmail.com>
2017-04-06 12:26:41 +0000

    add q4

---
+++ q4.js
@@ -0,0 +1,3 @@
+//4. Write the following programs:
+//  - Write a JavaScript program that will print every element in a, however, it will reverse the word for every other element.
+//  - Write the same program in Ruby.
